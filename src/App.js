6843eacfe69010ee2745ee8b82fb1c16482fc01d
xiaoyue hu <xiaoyuehu@xiaoyuedeMacBook-Pro.local>
2022-06-22 11:08:06 +0800

    redux added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { Counter } from './counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
